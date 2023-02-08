c4467814bc9660d19c0b48c77cc244642c35c996
Thinh Nguyen <gaupoit@outlook.com>
2023-02-08 14:11:08 +0700

    Implement discord form

---
+++ components/discordForm.tsx
@@ -0,0 +1,50 @@
+import { useAccount, useSignMessage } from "@usedapp/react";
+import { useForm } from "react-hook-form";
+import { setCookie } from "cookies-next";
+
+type FormValues = {
+  walletAddress: string;
+  amount: number;
+};
+
+const DiscordForm = ({ url, pk }) => {
+  const { signMessage, data } = useSignMessage({
+    onSuccess: (mess, params) => {
+      setCookie("signature", mess, {
+        maxAge: 1000 * 60 * 5,
+      });
+
+      setCookie("address", pk, {
+        maxAge: 1000 * 60 * 5,
+      });
+    },
+  });
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      walletAddress: "",
+      amount: 0,
+    },
+  });
+
+  const onSubmit = (formValues: FormValues) => {
+    signMessage({
+      message: `link-discord-${pk}`,
+      signingPublicKey: pk,
+    });
+  };
+
+  return (
+    <div>
+      {data ? (
+        <a href={`${url}&signature=${data}`}>Link to Discord {url}</a>
+      ) : (
+        <form className="singer-form" onSubmit={handleSubmit(onSubmit)}>
+          <input type="hidden" />
+          <button type="submit">Sign to connect Discord</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default DiscordForm;
