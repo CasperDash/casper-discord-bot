f44b1e8b7ffd8751f6e07f7f3ed0e81241eda652
Thinh Nguyen <gaupoit@outlook.com>
2023-02-08 16:12:29 +0700

    Change style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 function HomePage() {
-  return <div>Welcome to Casper Discord Bot</div>;
+  return (
+    <div className="main">
+      <h3>Welcome to Casper Discord Bot</h3>
+    </div>
+  );
 }
 
 export default HomePage;
