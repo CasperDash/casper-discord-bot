8cb0eee55c65cc6eafb0cfb838b95192811302bd
Thinh Nguyen <gaupoit@outlook.com>
2023-02-08 16:12:44 +0700

    Apply Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { CapserDashConnector, CasperSignerConnector } from "@usedapp/core";
 import { CasperProvider, createClient } from "@usedapp/react";
+import "../css/style.css";
+import Layout from "../components/layout";
 
 const client = createClient({
   connectors: [new CasperSignerConnector({}), new CapserDashConnector({})],
@@ -7,10 +9,10 @@ const client = createClient({
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Layout>
       <CasperProvider client={client}>
         <Component {...pageProps} />
       </CasperProvider>
-    </>
+    </Layout>
   );
 }
