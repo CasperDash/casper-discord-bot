1aa190c7bbdb87230964df9e76fab9b447f8f1f6
Thinh Nguyen <gaupoit@outlook.com>
2023-03-05 16:55:48 +0700

    Uncomment

--- src/server.js
+++ src/server.js
@@ -46,39 +46,39 @@ app.get("/verified-role", async (req, res) => {
  * 3. Stores the OAuth2 Discord Tokens in Redis / Firestore
  * 4. Lets the user know it's all good and to go back to Discord
  */
-//  app.get('/discord-oauth-callback', async (req, res) => {
-//   try {
-//     // 1. Uses the code and state to acquire Discord OAuth2 tokens
-//     const code = req.query['code'];
-//     const discordState = req.query['state'];
+app.get("/discord-oauth-callback", async (req, res) => {
+  try {
+    // 1. Uses the code and state to acquire Discord OAuth2 tokens
+    const code = req.query["code"];
+    const discordState = req.query["state"];
 
-//     // make sure the state parameter exists
-//     const { clientState } = req.signedCookies;
-//     if (clientState !== discordState) {
-//       console.error('State verification failed.');
-//       return res.sendStatus(403);
-//     }
+    // make sure the state parameter exists
+    const { clientState } = req.signedCookies;
+    if (clientState !== discordState) {
+      console.error("State verification failed.");
+      return res.sendStatus(403);
+    }
 
-//     const tokens = await discord.getOAuthTokens(code);
+    const tokens = await discord.getOAuthTokens(code);
 
-//     // 2. Uses the Discord Access Token to fetch the user profile
-//     const meData = await discord.getUserData(tokens);
-//     const userId = meData.user.id;
-//     await storage.storeDiscordTokens(userId, {
-//       access_token: tokens.access_token,
-//       refresh_token: tokens.refresh_token,
-//       expires_at: Date.now() + tokens.expires_in * 1000,
-//     });
+    // 2. Uses the Discord Access Token to fetch the user profile
+    const meData = await discord.getUserData(tokens);
+    const userId = meData.user.id;
+    await storage.storeDiscordTokens(userId, {
+      access_token: tokens.access_token,
+      refresh_token: tokens.refresh_token,
+      expires_at: Date.now() + tokens.expires_in * 1000,
+    });
 
-//     // 3. Update the users metadata, assuming future updates will be posted to the `/update-metadata` endpoint
-//     await updateMetadata(userId);
+    // 3. Update the users metadata, assuming future updates will be posted to the `/update-metadata` endpoint
+    await updateMetadata(userId);
 
-//     res.send('You did it!  Now go back to Discord.');
-//   } catch (e) {
-//     console.error(e);
-//     res.sendStatus(500);
-//   }
-// });
+    res.send("You did it!  Now go back to Discord.");
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
 
 /**
  * Example route that would be invoked when an external data source changes.
