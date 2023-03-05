e03b7a969a9eca2b36d268c66ffdda57d55e4271
Thinh Nguyen <gaupoit@outlook.com>
2023-03-05 21:27:41 +0700

    Fix mongo model issue.

--- src/db/models/Entry.js
+++ src/db/models/Entry.js
@@ -1,26 +1,25 @@
 import mongoose from "mongoose";
 
-const EntryModel = mongoose.model(
-  "Entry",
-  new mongoose.Schema(
-    {
-      publicKey: {
-        type: String,
-        require: true,
+export default mongoose.models.Entry ||
+  mongoose.model(
+    "Entry",
+    new mongoose.Schema(
+      {
+        publicKey: {
+          type: String,
+          require: true,
+        },
+        userId: {
+          type: String,
+          require: true,
+        },
+        isHatcher: {
+          type: Boolean,
+          default: false,
+        },
       },
-      userId: {
-        type: String,
-        require: true,
-      },
-      isHatcher: {
-        type: Boolean,
-        default: false,
-      },
-    },
-    {
-      timestamps: true,
-    }
-  )
-);
-
-export default EntryModel;
+      {
+        timestamps: true,
+      }
+    )
+  );
