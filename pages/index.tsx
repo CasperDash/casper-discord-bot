0dfcd04e35f7e65def6ae7fbacee8e43db8523e3
Thinh Nguyen <gaupoit@outlook.com>
2023-06-21 16:25:19 +0700

    Add link to add bot

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ function HomePage() {
   return (
     <div className="main">
       <h3>Welcome to Casper Discord Bot</h3>
+      <h4>
+        <a href="https://discord.com/api/oauth2/authorize?client_id=1045948751145095258&permissions=309438973952&scope=applications.commands%20bot">
+          Add your Bot to Server
+        </a>
+      </h4>
     </div>
   );
 }
