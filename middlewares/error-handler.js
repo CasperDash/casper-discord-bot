5f1be54f57fc7f424527d1317dc72f49da67e250
Thinh Nguyen <gaupoit@outlook.com>
2023-06-28 16:07:02 +0700

    Implement error handler

---
+++ middlewares/error-handler.js
@@ -0,0 +1,12 @@
+const withErrorHandler = (fn) => async (req, res) => {
+  try {
+    return await fn(req, res);
+  } catch (err) {
+    console.log("ERROR", err);
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Oops, something went wrong!";
+    res.status(statusCode).json({ statusCode, message });
+  }
+};
+
+export default withErrorHandler;
