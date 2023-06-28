22992bfc4bc9b17c121c2a5b6a3de500c5daafa5
Thinh Nguyen <gaupoit@outlook.com>
2023-06-28 12:35:50 +0700

    nit

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -18,14 +18,14 @@ export default async function handler(req, res) {
 
     if (message.type === InteractionType.PING) {
       console.log("Handling Ping request");
-      res.json({
+      res.status(200).json({
         type: InteractionResponseType.PONG,
       });
     } else if (message.type === InteractionType.APPLICATION_COMMAND) {
       console.log(`Handling application command: ${message.data.name}`);
       switch (message.data.name.toLowerCase()) {
         case GET_PROFILE.name.toLowerCase(): {
-          response.send({
+          res.send({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
               content: "123",
