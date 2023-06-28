1e3c720174edb081e7eb113ddcc1a7f6596c3300
Thinh Nguyen <gaupoit@outlook.com>
2023-06-28 13:03:16 +0700

    add verify key

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -2,10 +2,20 @@ import {
   InteractionResponseType,
   InteractionType,
   verifyKeyMiddleware,
+  verifyKey,
 } from "discord-interactions";
 import loadConfig from "../../src/config.js";
 
 export default async function handler(req, res) {
+  const signature = req.get("X-Signature-Ed25519");
+  const timestamp = req.get("X-Signature-Timestamp");
+
+  const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+  if (!isValidRequest) {
+    res.status(401).send("Bad request signature");
+    throw new Error("Bad request signature");
+  }
+
   if (req.method === "POST") {
     // Process a POST request
     const config = loadConfig();
@@ -17,7 +27,7 @@ export default async function handler(req, res) {
     }
 
     if (message.type === InteractionType.PING) {
-      console.log("Handling Ping request");
+      console.log("Handling Ping request 123");
       return res.send({ type: InteractionResponseType.PONG });
     } else if (message.type === InteractionType.APPLICATION_COMMAND) {
       console.log(`Handling application command: ${message.data.name}`);
