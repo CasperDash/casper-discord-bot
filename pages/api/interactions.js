8ad0683be524bb67cf118bddc5fdbbe2aa5fce12
Thinh Nguyen <gaupoit@outlook.com>
2023-07-10 15:48:20 +0700

    nit

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -144,7 +144,7 @@ const handler = async (req, res, interaction) => {
                           value: user.id,
                         },
                         {
-                          value: "Whitelist",
+                          name: "Whitelist",
                           value: wlRoundMessage,
                         },
                       ],
