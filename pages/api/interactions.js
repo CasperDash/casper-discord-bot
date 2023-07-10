97a90d52c5daeb7baf0d6a69d891a38d867fad51
Thinh Nguyen <gaupoit@outlook.com>
2023-07-10 14:46:30 +0700

    debug

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -115,92 +115,98 @@ const handler = async (req, res, interaction) => {
           });
         }
         case CHECK_WL.name.toLocaleLowerCase(): {
-          const { id } = user;
-          await connectToDb();
-          const entry = Entry.where({ userId: id });
-          const { isWLWinner } = await entry.findOne();
-          if (entry) {
-            const wlRoundMessage = isWLWinner
-              ? ":white_check_mark: Your wallet is eligible to mint Eggs in WL Round on July 17th 20203"
-              : ":x: Your wallet is unqualified to min Eggs in WL Round. Please join us as Public Round on 27th 2023";
+          try {
+            const { id } = user;
+            console.log("Connecting to DB");
+            await connectToDb();
+            const entry = Entry.where({ userId: id });
+            console.log("User", id);
+            const { isWLWinner } = await entry.findOne();
+            if (entry) {
+              const wlRoundMessage = isWLWinner
+                ? ":white_check_mark: Your wallet is eligible to mint Eggs in WL Round on July 17th 20203"
+                : ":x: Your wallet is unqualified to min Eggs in WL Round. Please join us as Public Round on 27th 2023";
+              return res.status(200).json({
+                type: 4,
+                data: {
+                  embeds: [
+                    {
+                      color: 0x0099ff,
+                      author: {
+                        name: nick,
+                        // icon_url: "https://i.imgur.com/AfFp7pu.png",
+                        // url: "https://discord.js.org",
+                      },
+                      fields: [
+                        {
+                          name: "ID",
+                          value: user.id,
+                        },
+                        {
+                          value: "Whitelist Round Eligibility",
+                          value: wlRoundMessage,
+                        },
+                      ],
+                      timestamp: new Date().toISOString(),
+                      footer: {
+                        text: "EggForce",
+                        icon_url:
+                          "https://eggforce.io/static/media/eggforce--logo__ver2_color.1b48c729ba5a2f91b7cd.webp",
+                      },
+                    },
+                  ],
+                  components: [
+                    {
+                      type: MessageComponentTypes.ACTION_ROW,
+                      components: [
+                        {
+                          type: MessageComponentTypes.BUTTON,
+                          // Value for your app to identify the button
+                          // custom_id: "link_wallet",
+                          label: "Link Wallet",
+                          style: ButtonStyleTypes.LINK,
+                          url: "https://discord.casperdash.io/verify-wallet",
+                        },
+                      ],
+                    },
+                  ],
+                  flags: InteractionResponseFlags.EPHEMERAL,
+                },
+              });
+            }
+
+            const message = {
+              color: 0x0099ff,
+              author: {
+                name: nick,
+              },
+              fields: [
+                {
+                  name: "ID",
+                  value: user.id,
+                },
+                {
+                  name: "Whitelist Round Eligibility",
+                  value: `You have never registered the Whitelist Ticket. Claim it eggforce.io/world now.`,
+                },
+              ],
+              timestamp: new Date().toISOString(),
+              footer: {
+                text: "CasperDash",
+                icon_url: "https://assets.eggforce.io/casperdash.webp",
+              },
+            };
+
             return res.status(200).json({
               type: 4,
               data: {
-                embeds: [
-                  {
-                    color: 0x0099ff,
-                    author: {
-                      name: nick,
-                      // icon_url: "https://i.imgur.com/AfFp7pu.png",
-                      // url: "https://discord.js.org",
-                    },
-                    fields: [
-                      {
-                        name: "ID",
-                        value: user.id,
-                      },
-                      {
-                        value: "Whitelist Round Eligibility",
-                        value: wlRoundMessage,
-                      },
-                    ],
-                    timestamp: new Date().toISOString(),
-                    footer: {
-                      text: "EggForce",
-                      icon_url:
-                        "https://eggforce.io/static/media/eggforce--logo__ver2_color.1b48c729ba5a2f91b7cd.webp",
-                    },
-                  },
-                ],
-                components: [
-                  {
-                    type: MessageComponentTypes.ACTION_ROW,
-                    components: [
-                      {
-                        type: MessageComponentTypes.BUTTON,
-                        // Value for your app to identify the button
-                        // custom_id: "link_wallet",
-                        label: "Link Wallet",
-                        style: ButtonStyleTypes.LINK,
-                        url: "https://discord.casperdash.io/verify-wallet",
-                      },
-                    ],
-                  },
-                ],
+                embeds: [message],
                 flags: InteractionResponseFlags.EPHEMERAL,
               },
             });
+          } catch (error) {
+            console.error("Something is wrong", error);
           }
-
-          const message = {
-            color: 0x0099ff,
-            author: {
-              name: nick,
-            },
-            fields: [
-              {
-                name: "ID",
-                value: user.id,
-              },
-              {
-                name: "Whitelist Round Eligibility",
-                value: `You have never registered the Whitelist Ticket. Claim it eggforce.io/world now.`,
-              },
-            ],
-            timestamp: new Date().toISOString(),
-            footer: {
-              text: "CasperDash",
-              icon_url: "https://assets.eggforce.io/casperdash.webp",
-            },
-          };
-
-          return res.status(200).json({
-            type: 4,
-            data: {
-              embeds: [message],
-              flags: InteractionResponseFlags.EPHEMERAL,
-            },
-          });
         }
         default:
           console.error("Unknown Command");
